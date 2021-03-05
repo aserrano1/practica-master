2609a304620ead1351be36b3943b54ff9f6156c6
Miguel Mejia <jmejia@dcatech.com>
2021-03-05 11:22:59 -0500

    Creacion de la Api Master

---
+++ src/main/java/com/practica/master/AppPracticaMasterApplication.java
@@ -0,0 +1,13 @@
+package com.practica.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPracticaMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPracticaMasterApplication.class, args);
+    }
+
+}
